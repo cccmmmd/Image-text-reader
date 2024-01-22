854f44f0212c60c1fa431e513f018fcdaa2f3a35
cccmmmd <bblisme8b@gmail.com>
2024-01-22 23:11:35 +0800

    complete almost all functions

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     $("#thisButton").click(function(){
         processImage();
     });
+    $(".send").click(function(){
+        sendToSheet();
+    });
     $("#inputImageFile").change(function(e){
       processImage(e.target.files[0]);
     });
@@ -10,7 +13,10 @@ $(document).ready(function(){
 
 function processImage(imageO ) {
   document.querySelector("#sourceImage").src = "";
-  $("#textResult").html("");
+  $('.send').hide();
+  let img = document.createElement("img");
+  img.src = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmx4bTNuemRveHU5bWN1OHl4OW9oMGI0bDB0NjNwc3RtMTFkbzZ1MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oEjI6SIIHBdRxXI40/giphy.gif";
+  $("#textResult").append(img);
     // **********************************************
     // *** Update or verify the following values. ***
     // **********************************************
@@ -97,11 +103,6 @@ function processImage(imageO ) {
     }
     document.querySelector("#sourceImage").src = sourceImageUrl;
 
-    // This operation requires two REST API calls. One to submit the image
-    // for processing, the other to retrieve the text found in the image.
-    //
-    // Make the first REST API call to submit the image for processing.
-
 };
 function fetchText(operationLocation) {
   $.ajax({
@@ -122,11 +123,72 @@ function fetchText(operationLocation) {
         fetchText(operationLocation);
       }
       let recognitionArray = data.analyzeResult.readResults[0].lines;
-      let str = "";
+      let myNode = document.querySelector("#textResult");
+      $("#textResult").html("");
       for(let x=0; x<recognitionArray.length;x++){
-        str += recognitionArray[x].text + '<br/>'
+        let myTable = document.createElement("tr");
+        myTable.innerHTML = '<td class="text">'+recognitionArray[x].text+'</td>'
+        let myTd = document.createElement("td");
+        let mySe = document.createElement("select");
+        let myOp = []
+
+        myOp[0] = document.createElement("option");
+        myOp[1] = document.createElement("option");
+        myOp[2] = document.createElement("option");
+        myOp[3] = document.createElement("option");
+        myOp[4] = document.createElement("option");
+        myOp[5] = document.createElement("option");
+        myOp[6] = document.createElement("option");
+        myOp[7] = document.createElement("option");
+        myOp[8] = document.createElement("option");
+        myOp[9] = document.createElement("option");
+        myOp[10] = document.createElement("option");
+        myOp[11] = document.createElement("option");
+        myOp[12] = document.createElement("option");
+        myOp[13] = document.createElement("option");
+        myOp[14] = document.createElement("option");
+
+        myOp[0].value = '';
+        myOp[1].value = 'cname';
+        myOp[2].value = 'ename';
+        myOp[3].value = 'title';
+        myOp[4].value = 'etitle';
+        myOp[5].value = 'department';
+        myOp[6].value = 'edepartment';
+        myOp[7].value = 'mobile';
+        myOp[8].value = 'email';
+        myOp[9].value = 'phone';
+        myOp[10].value = 'fax';
+        myOp[11].value = 'company';
+        myOp[12].value = 'address';
+        myOp[13].value = 'eaddress';
+        myOp[14].value = 'businessID';
+
+        myOp[0].innerHTML = '請選擇類別';
+        myOp[1].innerHTML = '中文姓名';
+        myOp[2].innerHTML = '英文姓名';
+        myOp[3].innerHTML = '職稱';
+        myOp[4].innerHTML = '英文職稱';
+        myOp[5].innerHTML = '部門';
+        myOp[6].innerHTML = '部門英文';
+        myOp[7].innerHTML = '手機';
+        myOp[8].innerHTML = 'email';
+        myOp[9].innerHTML = '電話';
+        myOp[10].innerHTML = '傳真';
+        myOp[11].innerHTML = '公司名稱';
+        myOp[12].innerHTML = '公司地址 ';
+        myOp[13].innerHTML = '公司英文地址';
+        myOp[14].innerHTML = '統編';
+
+        for(var i=0; i<myOp.length; i++) {
+            mySe.appendChild(myOp[i]);
+        }
+        myTd.appendChild(mySe)
+        myTable.appendChild(myTd);
+        myNode.appendChild(myTable);
       }
-      $("#textResult").html(str);
+      $('.send').show();
+      // $("#textResult").html(str);
 
   })
   .fail(function(jqXHR, textStatus, errorThrown) {
@@ -139,4 +201,33 @@ function fetchText(operationLocation) {
               jQuery.parseJSON(jqXHR.responseText).error.message;
       alert(errorString);
   });
+}
+function sendToSheet(){
+  let myNode = document.querySelectorAll('tr');
+  let oneData = {'id': "INCREMENT"}
+  let passcheck = false;
+  for (var i = 0; i < myNode.length; i++) {
+    let key = myNode[i].querySelector('select').value;
+    let value = myNode[i].querySelector('.text').innerText;
+    if(key == '') {
+      alert('請確認每個資料都已選擇類別！');
+      break;
+    }else{
+      oneData[key] = value;
+    }
+  }
+  fetch('https://sheetdb.io/api/v1/6helj6tgbivyp', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          data: oneData
+      })
+  })
+    .then((response) => alert("記錄成功！"))
+    .then((data) => console.log(data))
+
+
 }
