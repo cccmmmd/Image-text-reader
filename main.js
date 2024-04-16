206010240804fc67b4d8c98e16677aef9be85771
Barbie Lee <137893455+cccmmmd@users.noreply.github.com>
2024-04-16 21:34:14 +0800

    renew layout

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ function processImage(imageO ) {
     // **********************************************
     // *** Update or verify the following values. ***
     // **********************************************
-    // $("#RecognitionCardNumber").text("XXXX XXXX XXXX XXXX");
-    let endpoint = "https://switzerlandnorth.api.cognitive.microsoft.com/";
+    
+    let endpoint = "https://westus.api.cognitive.microsoft.com/";
     if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
 
     var uriBase = endpoint + "vision/v3.2/read/analyze";
@@ -128,7 +128,8 @@ function fetchText(operationLocation) {
         let myTable = document.createElement("tr");
         myTable.innerHTML = '<td class="text">'+recognitionArray[x].text+'</td>'
         let myTd = document.createElement("td");
-        let mySe = document.createElement("select");
+        let mySe = document.createElement("select")
+        mySe.classList.add("form-select")
         let myOp = []
 
         myOp[0] = document.createElement("option");
@@ -143,41 +144,32 @@ function fetchText(operationLocation) {
         myOp[9] = document.createElement("option");
         myOp[10] = document.createElement("option");
         myOp[11] = document.createElement("option");
-        myOp[12] = document.createElement("option");
-        myOp[13] = document.createElement("option");
-        myOp[14] = document.createElement("option");
 
         myOp[0].value = '';
         myOp[1].value = 'cname';
         myOp[2].value = 'ename';
         myOp[3].value = 'title';
-        myOp[4].value = 'etitle';
-        myOp[5].value = 'department';
-        myOp[6].value = 'edepartment';
-        myOp[7].value = 'mobile';
-        myOp[8].value = 'email';
-        myOp[9].value = 'phone';
-        myOp[10].value = 'fax';
-        myOp[11].value = 'company';
-        myOp[12].value = 'address';
-        myOp[13].value = 'eaddress';
-        myOp[14].value = 'businessID';
+        myOp[4].value = 'department';
+        myOp[5].value = 'mobile';
+        myOp[6].value = 'email';
+        myOp[7].value = 'phone';
+        myOp[8].value = 'fax';
+        myOp[9].value = 'company';
+        myOp[10].value = 'address';
+        myOp[11].value = 'businessID';
 
         myOp[0].innerHTML = '請選擇類別';
         myOp[1].innerHTML = '中文姓名';
         myOp[2].innerHTML = '英文姓名';
         myOp[3].innerHTML = '職稱';
-        myOp[4].innerHTML = '英文職稱';
-        myOp[5].innerHTML = '部門';
-        myOp[6].innerHTML = '部門英文';
-        myOp[7].innerHTML = '手機';
-        myOp[8].innerHTML = 'email';
-        myOp[9].innerHTML = '電話';
-        myOp[10].innerHTML = '傳真';
-        myOp[11].innerHTML = '公司名稱';
-        myOp[12].innerHTML = '公司地址 ';
-        myOp[13].innerHTML = '公司英文地址';
-        myOp[14].innerHTML = '統編';
+        myOp[4].innerHTML = '部門';
+        myOp[5].innerHTML = '手機';
+        myOp[6].innerHTML = 'email';
+        myOp[7].innerHTML = '電話';
+        myOp[8].innerHTML = '傳真';
+        myOp[9].innerHTML = '公司名稱';
+        myOp[10].innerHTML = '公司地址 ';
+        myOp[11].innerHTML = '統編';
 
         for(var i=0; i<myOp.length; i++) {
             mySe.appendChild(myOp[i]);
